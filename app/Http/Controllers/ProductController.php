f7a61c6935e760b577638abc69f5ea8a4d6f8c6f
Ahmad Maarouf <a.maarouf@turnsole.tech>
2022-04-27 17:21:01 +0300

    update api

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -72,7 +72,10 @@ class ProductController extends Controller
      */
     public function update(UpdateProductRequest $request, Product $product)
     {
-        //
+        $product->update($request->all());
+        return response([
+            'data'=>new ProductResource($product),
+        ],HttpFoundationResponse::HTTP_CREATED);
     }
 
     /**
